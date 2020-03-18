acdce959166d37049c74ad2759784ab08c63623f
travis88 <hellsballs99@gmail.com>
2020-03-18 07:15:20 +0300

    more balls!

--- core/src/com/mygdx/game/MyGdxGame.kt
+++ core/src/com/mygdx/game/MyGdxGame.kt
@@ -3,25 +3,35 @@ package com.mygdx.game
 import com.badlogic.gdx.ApplicationAdapter
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.GL20
-import com.badlogic.gdx.graphics.Texture
-import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer
 import com.mygdx.game.entity.Ball
+import java.util.*
+import kotlin.collections.ArrayList
 
 class MyGdxGame : ApplicationAdapter() {
     lateinit var shape: ShapeRenderer
-    lateinit var ball: Ball
-    
+    lateinit var balls: ArrayList<Ball>
+    var rand = Random()
+
     override fun create() {
         shape = ShapeRenderer()
-        ball = Ball(150f, 200f, 70f, 12, 5)
+        balls = ArrayList()
+        for (i in 0..10) {
+            balls.add(Ball(rand.nextInt(Gdx.graphics.width).toFloat(),
+                           rand.nextInt(Gdx.graphics.height).toFloat(),
+                           rand.nextInt(100).toFloat(),
+                           rand.nextInt(15),
+                           rand.nextInt(15)))
+        }
     }
 
     override fun render() {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
-        ball.update()
         shape.begin(ShapeRenderer.ShapeType.Filled)
-        ball.draw(shape)
+        for (ball in balls) {
+            ball.update()
+            ball.draw(shape)
+        }
         shape.end()
     }
 }
